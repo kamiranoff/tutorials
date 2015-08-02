18fe852403b6463cd8fcd5905108f73a50dde808
Kevin Amiranoff <kamiranoff@gmail.com>
2015-08-02 23:38:53 +0200

    little bit of refactoring

--- server/chat/chatController.js
+++ server/chat/chatController.js
@@ -1,38 +1,53 @@
 /**
-*
-* client side on client/scripts/chat/chat.js
-*
-**/
+ *
+ * client side on client/scripts/chat/chat.js
+ *
+ **/
+
+//socket.emit : emit to the active user
+//socket.broadcast.emit : emit to all user except active one
+//io.sockets.emit : emit to all users
 
 
 var mongo = require('mongodb').MongoClient,
-    io = require('socket.io')(8080).sockets,
-    util = require('util'),
-    validator = require('validator');
+  io = require('socket.io')(8080).sockets,
+  util = require('util'),
+  validator = require('validator');
 
 module.exports = function(env, logger) {
 
 
   logger.info('Database', env.chatdb);
 
+  var logAndUpdateStatus = function(socket, warning, statusMessage) {
+    logger.warn(warning);
+    sendStatus(socket, statusMessage);
+  };
+
+  var sendStatus = function(socket, string) {
+    socket.emit('status', string);
+  };
+
+
+
   mongo.connect(env.chatdb, function(err, db) {
     if (err) {
       throw err;
     }
+
     io.on('connection', function(socket) {
       logger.info('connected to db chat');
 
-      var messageCollection = db.collection('messages'),
-          sendStatus = function(string) {
-          socket.emit('status', string);
-      };
+      var messageCollection = db.collection('messages');
 
       /*==========  Emit all messages  ==========*/
-      messageCollection.find().limit(100).sort({ _id:1}).toArray(function(err,res){
-        if(err){
+      messageCollection.find().limit(100).sort({
+        _id: 1
+      }).toArray(function(err, res) {
+        if (err) {
           throw err;
         }
-        socket.emit('output',res);
+        socket.emit('output', res);
       });
 
 
@@ -42,41 +57,39 @@ module.exports = function(env, logger) {
         //stripLow(input [, keep_new_lines]) - remove characters with a numerical value < 32 and 127, mostly control characters.
         //If keep_new_lines is true, newline characters are preserved (\n and \r, hex 0xA and 0xD). Unicode-safe in JavaScript.
         var name = validator.stripLow(data.name),
-          message = validator.stripLow(data.message,true),
-          whiteSpacePattern = /^\s*$/;
+          message = validator.stripLow(data.message, true),
+          whiteSpacePattern = /^\s*$/,
+          messageObject = {};
 
 
         //checking for white spaces
         if (whiteSpacePattern.test(name) || whiteSpacePattern.test(message)) {
-          logger.warn('white space only found. Not inserting that in the db. Sorry');
-          sendStatus('A name and a message is required');
+          logAndUpdateStatus(socket, 'white space only found. Not inserting that in the db. Sorry', 'A name and a message is required');
 
+        } else if (!validator.isLength(name, 1, 50)) {
+          logAndUpdateStatus(socket, 'Name is too long. Not inserting that in the db. Sorry', 'Your name is either too short or too long');
 
-        }else if(!validator.isLength(name,1,50)){
-          logger.warn('Name is too long. Not inserting that in the db. Sorry');
-          sendStatus('Your name is either too short or too long');
+        } else if (!validator.isLength(message, 1, 300)) {
+          logAndUpdateStatus(socket, 'Message is too long. Not inserting that in the db. Sorry', 'Your message is either too short or too long');
 
-        }else if(!validator.isLength(message,1,300)){
-          logger.warn('Message is too long. Not inserting that in the db. Sorry');
-          sendStatus('Your message is either too short or too long');
-
-
-        //insert message to db and send response to client
+          //insert message to db and send response to client
         } else {
-          //validation before saving into db;
-          var messageObject = {
+          //has passed validation
+
+          messageObject = {
             name: name,
             message: message
           };
-         messageCollection.insert(messageObject, function() {
+
+          messageCollection.insert(messageObject, function() {
             logger.info('inserted: ' + message);
 
             //Emit latest message to ALL clients
-            io.emit('output',[data]);
+            io.emit('output', [data]);
 
-            sendStatus({
-              message:'Message sent',
-              clear:true
+            sendStatus(socket, {
+              message: 'Message sent',
+              clear: true
             });
 
           });
